c74b9d85baca25e6aea8a15aec4a2813153bef6d
markd <mark.permyakov.2002@bk.ru>
2020-09-30 16:35:38 +0300

    added functions:   is_element_loaded(element) - checks if element is in RAM[] or not   load_page - load page in RAM[] if it is not in yet

--- virtual_memory.py
+++ virtual_memory.py
@@ -1,15 +1,29 @@
 with open('RAM_frames.txt') as file:
     M = int(file.readline())
-    RAM = [None]*M
+    RAM = [0]*M
+
+
+def is_element_loaded(element):
+    for j in range(M):
+        if RAM[j] == element:
+            return 'YES'
+    return 'NO'
+
+
+def load_page(page, k):
+    if is_element_loaded(page) == 'NO':
+        RAM[k] = page
+
 
 with open('page_references.txt') as file:
-    N = file.readline()
+    N = int(file.readline())
     addres_space = []
-    for element in file.readline().strip().split():
-        addres_space.append(int(element))
+    for number in file.readline().strip().split():
+        addres_space.append(int(number))
 
 
 for i in range(M):
-    RAM[i] = addres_space[i]
+    load_page(addres_space[i], i)
 
 
+print(RAM)
