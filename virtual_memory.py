1744d6a966ddd6c705ccc170b458b1ddef673d6a
markd <mark.permyakov.2002@bk.ru>
2020-09-30 22:54:57 +0300

    commentary is added

--- virtual_memory.py
+++ virtual_memory.py
@@ -2,10 +2,13 @@ with open('RAM_frames.txt') as file:
     M = int(file.readline())
     RAM = [-1]*M
 
+# массив, изображающий оперативную память
+
 
 def refresh(array):
     for n in array:
         n = -1
+# функция, обновления массива
 
 
 def is_element_loaded(element):
@@ -13,11 +16,13 @@ def is_element_loaded(element):
         if RAM[j] == element:
             return 'YES'
     return 'NO'
+# проверка на наличие того или иного элемента в массиве
 
 
 def load_page(page, k):
     if is_element_loaded(page) == 'NO':
         RAM[k] = page
+# функция, загружающая элемент в массив, если его там еще нет
 
 
 with open('page_references.txt') as file:
@@ -25,14 +30,18 @@ with open('page_references.txt') as file:
     addres_space = []
     for number in file.readline().strip().split():
         addres_space.append(int(number))
+# заполнили массив страничного пространства
 
 
 def fifo(fifo_answers=0):
     refresh(RAM)
     used_frames = 0
+    # количество уже занятых кадров памяти
     vacant_frame = 0
+    # индекс первого свободного кадра памяти
     for i in range(N):
         if used_frames < M:
+            # пока в каждом кадре не будет по странице
             if is_element_loaded(addres_space[i]) == 'NO':
                 load_page(addres_space[i], vacant_frame)
                 used_frames += 1
@@ -42,8 +51,9 @@ def fifo(fifo_answers=0):
             if is_element_loaded(addres_space[i]) == 'NO':
                 del RAM[0]
                 RAM.insert(M-1, addres_space[i])
+                # удаляем первый элемент, в конец ставим новый, тем самым сдвинув массив на один влево
                 fifo_answers += 1
-    return fifo_answers
+    return fifo_answers  # количество ответов второго типа
 
 
 def lru(lru_answers=0):
@@ -60,6 +70,7 @@ def lru(lru_answers=0):
             else:
                 RAM.insert(vacant_frame, addres_space[i])
                 RAM.pop(addres_space[i])
+                # так как страница только что вызвалась, переставляем её вправо в свободный кадр
         else:
             if is_element_loaded(addres_space[i]) == 'NO':
                 del RAM[0]
@@ -68,7 +79,8 @@ def lru(lru_answers=0):
             else:
                 RAM.insert(vacant_frame, addres_space[i])
                 RAM.pop(addres_space[i])
-    return lru_answers
+                # так как страница только что вызвалась, переставляем её в конец массива как самую новую
+    return lru_answers # количество ответов второго типа
 
 
 print(fifo())
